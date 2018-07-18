0a98241b54b1ebd27301f5a5b730a2f36702af2d
Mantecorso <luisjuradoquesada@gmail.com>
2018-07-18 22:01:08 +0200

    anyadido imagenes finametrix, popper.js con bower y enlazado con el layout

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('hbs');
+var hbsUtils = require('hbs-utils')(hbs);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+hbs.registerPartials(`${__dirname}/views/partials`);
+hbsUtils.registerPartials(`${__dirname}/views/partials`);
+hbsUtils.registerWatchedPartials(`${__dirname}/views/partials`);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
